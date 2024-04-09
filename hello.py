569ffa26f2db2f80fd89fde9d437fc8f7afdf14d
Derry Pratama <derryprata@gmail.com>
2024-04-09 10:45:03 +0900

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+def main():
+    # Prompt the user to enter their name
+    name = input("Please enter your name: )
+    
+    # Print the entered name
+    print("Hello," name)
+
+if __name__ == "__main__":
+    main()
